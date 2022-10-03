e16cd6cc5694418049357dd618e2e48ed616f6fc
Pease Adeniji <adenijiayomide13@gmail.com>
2022-10-03 03:40:53 +0100

    react native nft-marketplace

---
+++ components/index.js
@@ -0,0 +1,8 @@
+ import NavBar from "./NavBar"
+ import NftCard from "./NftCard"
+ import FocusedStatusBar from "./FocusedStatusBar"
+ import { CircleButton, RectButton  } from "./Button"
+
+ export  {
+    NavBar, NftCard, FocusedStatusBar, CircleButton, RectButton
+ }
